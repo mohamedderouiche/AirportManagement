acfe66641c1e06f91f1b0af9fd5fdf452678b7cb
mohamedderouiche <mohamedamine.derouiche@esprit.tn>
2024-05-04 19:30:07 +0100

    update

---
+++ AM.ApplicationCore/Interfaces/IServiceTicket.cs
@@ -0,0 +1,16 @@
+﻿using AM.ApplicationCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Interfaces
+{
+   public interface IServiceTicket: IService<Ticket>
+    {
+        IEnumerable <Ticket> GetTicketByPrice(string Prix);
+
+        IEnumerable<Ticket> GetTicketByPriceAndChair(string? prix, string? chairNbre);
+    }
+}
---
+++ AM.ApplicationCore/Services/ServiceTicket.cs
@@ -0,0 +1,44 @@
+﻿using AM.ApplicationCore.Services;
+using AM.ApplicationCore.Interfaces;
+using AM.ApplicationCore.Domain;
+using System.Text.RegularExpressions;
+
+namespace AM.ApplicationCore.Interfaces
+{
+
+    public class ServiceTicket : Service<Ticket>, IServiceTicket
+    {
+       
+        public ServiceTicket(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public IEnumerable<Ticket> GetTicketByPrice(string Prix)
+        {
+            return GetMany().Where(t => t.Prix >= float.Parse(Prix));
+        }
+
+        public IEnumerable<Ticket> GetTicketByPriceAndChair(string? prix, string? chairNbre)
+        {
+         
+            if(prix != null && chairNbre != null)
+            {
+                return GetMany().Where(t => t.Prix >= float.Parse(prix) && t.Siege >= int.Parse(chairNbre));
+            }
+            if (prix != null && chairNbre == null)
+            {
+                return GetMany(t => t.Prix >= float.Parse(prix));
+            }
+            if(chairNbre != null && prix == null )
+            {
+                return GetMany().Where(t => t.Siege >= int.Parse(chairNbre));
+            }
+            else 
+            {
+                return GetMany();
+            }
+        }
+
+        
+    }
+}
